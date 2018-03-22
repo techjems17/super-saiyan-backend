b8dbe2f3a5c1a925e71aecc805be07b1cfc4a860
Jordan Smith <techjems17@gmail.com>
2018-03-22 13:36:21 -0600

    added tables to migrations

--- migrations/20180322133110_profiles.js
+++ migrations/20180322133110_profiles.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('profiles', table => {
+        table.increments();
+        table.string("name");
+        table.string('dob');
+        table.string('description');
+        table.string('teacher');
+        table.string('image');
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('profiles')
 };
--- migrations/20180322133323_skills.js
+++ migrations/20180322133323_skills.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('skills', skill => {
+        skill.increments();
+        skill.string("name");
+        skill.string('description');
+    });
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('skills')
 };
