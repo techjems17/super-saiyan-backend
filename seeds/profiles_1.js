9d3860fb92a7f431f1f979bcb8ca590d9aac291f
Jordan Smith <techjems17@gmail.com>
2018-03-22 14:22:04 -0600

    updated seeds

--- seeds/profiles_1.js
+++ seeds/profiles_1.js
@@ -1,13 +1,27 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex, Promise) {
+  return knex.raw('TRUNCATE TABLE profiles RESTART IDENTITY CASCADE')
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('profiles').insert([{
+        name: "Jordan Smith",
+        dob: "12-17-1995",
+        description: "Be better than Goku",
+        teacher: "Vegeta",
+        image: "http://www.pngmart.com/files/2/Vegeta-PNG-Image.png"
+      },
+      {
+        name: "Isaiah Martinez",
+        dob: "10-28-1995",
+        description: "Save the World",
+        teacher: "Goku",
+        image: "http://www.pngmart.com/files/2/Goku-Transparent-PNG.png"
+      },
+      {
+        name: "Josh Swinger",
+        dob: "04-16-1992",
+        description: "Look awesome old",
+        teacher: "Master Roshi",
+        image: "https://vignette.wikia.nocookie.net/drslump/images/f/f3/517557-tortue_gniale.png/revision/latest?cb=20140121194858"
+      }
       ]);
     });
 };
--- seeds/skills_2.js
+++ seeds/skills_2.js
@@ -1,13 +1,18 @@
-
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
+exports.seed = function (knex, Promise) {
+  return knex.raw('TRUNCATE TABLE skills RESTART IDENTITY CASCADE')
     .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('skills').insert([{
+        name: "Superhuman speed",
+        description: "Travel great distances in short periods of time, or move at untrackable speeds while in combat."
+      },
+      {
+        name: "Superhuman strength",
+        description: "Strong enough to effortlessly break all Earthling-made materials or weapons, move in increased gravity without any discomfort, and even lift a suit capable of sinking through a planet."
+      },
+      {
+        name: "Superhuman senses",
+        description: "Able to think and react at extremely high speeds, easily able to dodge or catch projectiles fired from weapons, or even keep up with incredible combat speeds from opponents."
+      }
       ]);
     });
 };
